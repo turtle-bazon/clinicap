fb935745e1e2f24c27e09e98f2e91dae855ff80e
Azamat S. Kalimoulline <turtle@bazon.ru>
2012-02-25 23:56:16 +0400

    Tests for ini.

--- tests/test-ini.lisp
+++ tests/test-ini.lisp
@@ -16,3 +16,13 @@
     (add-ini-section root-ini "general")
     (ensure-same 1 (length (ini-sections root-ini)))
     (ensure-same "general" (ini-name (first (ini-sections root-ini))))))
+
+(addtest
+    test-ini-property
+  (let ((root-ini (make-ini :name "ROOT")))
+    (set-ini-property root-ini '("general" "max-connections") 20)
+    (ensure-same 20 (get-ini-property root-ini '("general" "max-connections")))
+    (ensure-same 1 (length (ini-sections root-ini)))
+    (let ((general-section (first (ini-sections root-ini))))
+      (ensure-same 1 (length (ini-properties general-section)))
+      (ensure-same 20 (first (ini-properties general-section))))))
